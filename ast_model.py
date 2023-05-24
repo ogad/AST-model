75ea5ca9de146bedf641b889d03c3c4928cdacad
Oliver <ogadriver147@gmail.com>
2023-05-24 14:46:27 +0100

    Add circle object generation

--- ast_model.py
+++ ast_model.py
@@ -7,6 +7,7 @@ from dataclasses import dataclass
 
 # Package imports
 import numpy as np
+from matplotlib import pyplot as plt
 
 
 class IntensityField(np.ndarray):
@@ -16,13 +17,25 @@ class IntensityField(np.ndarray):
         np.ndarray: The intensity field
     """
 
-    def plot(self, ax, **kwargs):
+    def plot(self, ax=None, diameter_px=None, **kwargs):
         """Plot the intensity field
 
         Args:
             ax (matplotlib.axes.Axes): The axis to plot on.
+            diameter(float): The diameter of the object in micrometres.
         """
-        ax.imshow(self, **kwargs)
+        if ax is None:
+            fig, ax = plt.subplots()
+        if diameter_px is not None:
+            # diameter_px = diameter * 1e-6 / pixel_size
+            to_plot = self[
+                (self.shape[0] - diameter_px) // 2 : (self.shape[0] + diameter_px) // 2,
+                (self.shape[1] - diameter_px) // 2 : (self.shape[1] + diameter_px) // 2,
+            ]
+        else:
+            to_plot = self
+
+        return ax.imshow(to_plot, **kwargs)
 
     def n_pixels_depletion_range(self, min_dep, max_dep):
         """Calculate the number of pixels in the depletion range
@@ -51,15 +64,44 @@ class ASTModel:
     Args:
         opaque_shape (np.ndarray): The shape of the opaque object in the z=0 plane.
         wavenumber (float, optional): The wavenumber of the light. Defaults to 2 * np.pi / (658 nm).
-        pizel_size (float, optional): The size of the pixels in the opaque_shape. Defaults to 10 µm.
+        pixel_size (float, optional): The size of the pixels in the opaque_shape. Defaults to 10 µm.
     """
 
     opaque_shape: np.ndarray
     wavenumber: float = np.pi / 658e-9  # in inverse metres
-    pizel_size: float = 10e-6  # in metres
+    pixel_size: float = 10e-6  # in metres
 
     def __post_init__(self):
         self.intenisties = {}  # z: intenisty grid
+
+    @classmethod
+    def from_diameter(cls, diameter, wavenumber=None, pixel_size=None):
+        """Create a model for a circular opaque object.
+
+        Args:
+            diameter (float): The diameter of the opaque object in micrometres.
+            wavenumber (float, optional): The wavenumber of the light. Defaults to 2 * np.pi / (658 nm).
+            pixel_size (float, optional): The size of the pixels in the opaque_shape. Defaults to 10 µm.
+
+        Returns:
+            ASTModel: The model.
+        """
+        # set defaults
+        if wavenumber is None:
+            wavenumber = cls.wavenumber
+        if pixel_size is None:
+            pixel_size = cls.pixel_size
+
+        # create the opaque shape
+        radius_m = diameter * 1e-6 / 2
+        radius_px = radius_m / pixel_size
+        x = np.arange(-radius_px, radius_px)
+        y = np.arange(-radius_px, radius_px)
+        xx, yy = np.meshgrid(x, y)
+        opaque_shape = np.where(xx**2 + yy**2 <= radius_px**2, 1, 0)
+
+        # create the model
+        return cls(opaque_shape, wavenumber, pixel_size)
 
     def process(self, z_val: int) -> IntensityField:
         """Process the model for a given z
@@ -82,8 +124,8 @@ class ASTModel:
         transmission_function_fourier = np.fft.fft2(transmission_function)
 
         # calculate the fourier space coordinates
-        f_x = np.fft.fftfreq(transmission_function.shape[0], self.pizel_size)
-        f_y = np.fft.fftfreq(transmission_function.shape[1], self.pizel_size).reshape(
+        f_x = np.fft.fftfreq(transmission_function.shape[0], self.pixel_size)
+        f_y = np.fft.fftfreq(transmission_function.shape[1], self.pixel_size).reshape(
             -1, 1
         )
 
@@ -121,3 +163,13 @@ class ASTModel:
             np.ndarray: The intensity of the image at z.
         """
         return [self.process(z) for z in z_range]
+
+    def plot_intensity(self, z_val: int):
+        """Plot the intensity at a given z
+
+        Args:
+            z (float): The distance of the the opaque_shape from the object plane.
+        """
+        intensity = self.process(z_val)
+
+        intensity.plot()
--- demo_ast_model.py
+++ demo_ast_model.py
@@ -19,29 +19,31 @@ from ast_model import ASTModel
 # im = imread("rosette_basic.png")
 # image = np.where(im.max(axis=2), 1, 0)
 
-## circle object
-radius = 6  # in 10s of microns
-image = np.zeros((16, 16))
-for xx in range(16):
-    for yy in range(16):
-        if (xx - 8) ** 2 + (yy - 8) ** 2 < radius**2:
-            image[xx, yy] = 1
-image_shape = image.shape
+# ## circle object
+radius = 60  # in 10s of microns
+# image = np.zeros((16, 16))
+# for xx in range(16):
+#     for yy in range(16):
+#         if (xx - 8) ** 2 + (yy - 8) ** 2 < radius**2:
+#             image[xx, yy] = 1
+# image_shape = image.shape
 
-expanded_image = np.zeros((512, 512))
-# place image in the centre of the expanded image
-start_image_x = 256 - image_shape[0] // 2
-start_image_y = 256 - image_shape[1] // 2
-expanded_image[
-    start_image_x : start_image_x + image_shape[0],
-    start_image_y : start_image_y + image_shape[1],
-] = image
+# expanded_image = np.zeros((512, 512))
+# # place image in the centre of the expanded image
+# start_image_x = 256 - image_shape[0] // 2
+# start_image_y = 256 - image_shape[1] // 2
+# expanded_image[
+#     start_image_x : start_image_x + image_shape[0],
+#     start_image_y : start_image_y + image_shape[1],
+# ] = image
 
-# create the model
-model = ASTModel(opaque_shape=expanded_image)
+# # create the model
+# model = ASTModel(opaque_shape=expanded_image)
+model = ASTModel.from_diameter(radius * 2)
 
 # calculate the intensity at different z values
 z = 2e-3  # in metres
+image = model.process(0)
 intensity = model.process(z)
 
 # Plotting
@@ -52,18 +54,14 @@ intensity_norm = TwoSlopeNorm(vmin=0, vcenter=1, vmax=2)
 fig, (ax1, ax2) = plt.subplots(1, 2)
 
 ## Plot the object on first axis
-ax1.imshow(1 - image, norm=intensity_norm, cmap="BrBG_r")
+image.plot(ax=ax1, diameter_px=25, norm=intensity_norm, cmap="BrBG_r")
 ax1.set_title("Object at z = 0mm")
 ax1.set_xlabel("x/10µm")
 ax1.set_ylabel("y/10µm")
 
 ## Plot the diffracted intensity on second axis
-intensity_unextended = intensity[
-    start_image_x : start_image_x + image_shape[0],
-    start_image_y : start_image_y + image_shape[1],
-]
 # a = ax2.imshow(intensity_unextended, norm=intensity_norm, cmap="BrBG_r")
-a = intensity_unextended.plot(ax=ax2, norm=intensity_norm, cmap="BrBG_r")
+a = intensity.plot(ax=ax2, diameter_px=25, norm=intensity_norm, cmap="BrBG_r")
 ax2.set_title(f"Intensity at z = {z*1000}mm")
 ax2.set_xlabel("x/10µm")
 ax2.set_ylabel("y/10µm")
