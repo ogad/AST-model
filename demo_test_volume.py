41373d079e5aa457f39ab5d4278298095c46a28b
Oliver <ogadriver147@gmail.com>
2023-07-24 11:10:58 +0100

    Simplify figures

--- demo_test_volume.py
+++ demo_test_volume.py
@@ -82,42 +82,38 @@ def make_run(shape, distance, n_px, plot=True):
 # detections.amplitude.intensity.plot()
 
 def make_and_plot_retrievals(run):
-    retrieval2 = Retrieval(run, DiameterSpec(diameter_method="xy", min_sep=5e-4, filled=True))
+    # retrieval2 = Retrieval(run, DiameterSpec(diameter_method="xy", min_sep=5e-4, filled=True))
     retrieval = Retrieval(run, DiameterSpec(min_sep=5e-4))
 
     # retrieval = Retrieval(run, DiameterSpec(diameter_method="xy", min_sep=0.1, filled=True))
     fit = GammaPSD.fit(retrieval.midpoints, retrieval.dn_dd_measured, min_considered_diameter = 20e-6) # What minimum diameter is appropriate; how can we account for the low spike...
-    fit2 = GammaPSD.fit(retrieval2.midpoints, retrieval2.dn_dd_measured, min_considered_diameter = 20e-6)
+    # fit2 = GammaPSD.fit(retrieval2.midpoints, retrieval2.dn_dd_measured, min_considered_diameter = 20e-6)
 
-    fig, axs = plt.subplots(2, 2, width_ratios=[1, 5], height_ratios=[3,1], figsize=(10, 7), sharex='col')
+    fig, axs = plt.subplots(2, 1, height_ratios=[3,1], figsize=(7.2, 5), sharex='col')
 
-    psd_axs = axs[0,:]
+    ax = axs[0]
 
-    for ax in psd_axs:
-        true = cloud.psd.plot(ax, label=f"True\n{gamma_dist.parameter_description()}")
-        cloud.psd.plot(ax, label=f"True\n{gamma_dist.parameter_description()}", retrieval=retrieval, color="C1", linestyle="dotted")
-        cloud.psd.plot(ax, label=f"True\n{gamma_dist.parameter_description()}", retrieval=retrieval2, color="C2", linestyle="dotted")
-        retrieval.plot(label="Retrieved (Circ. equiv.)", ax=ax, color="C1")
-        retrieval2.plot(label="Retrieved (XY)", ax=ax, color="C2")
-        fit_ce = fit.plot(ax, label=f"Circle equivalent\n{fit.parameter_description()}", color="C1")
-        fit_xy = fit2.plot(ax, label=f"XY mean\n{fit2.parameter_description()}", color="C2")
+    true = cloud.psd.plot(ax, label=f"True\n{gamma_dist.parameter_description()}")
+    cloud.psd.plot(ax, label=f"True\n{gamma_dist.parameter_description()}", retrieval=retrieval, color="C0", linestyle="dotted")
+    # cloud.psd.plot(ax, label=f"True\n{gamma_dist.parameter_description()}", retrieval=retrieval2, color="C2", linestyle="dotted")
+    retrieval.plot(label="Retrieved (Circ. equiv.)", ax=ax, color="C1")
+    # retrieval2.plot(label="Retrieved (XY)", ax=ax, color="C2")
+    fit_ce = fit.plot(ax, label=f"Circle equivalent\n{fit.parameter_description()}", color="C1")
+    # fit_xy = fit2.plot(ax, label=f"XY mean\n{fit2.parameter_description()}", color="C2")
 
     # plt.yscale("log")
-    psd_axs[0].set_xlim(0, 1e-4)
-    psd_axs[0].set_xticks([0, 1e-4])
-    psd_axs[1].set_ylim(0, 0.5e9)
-    psd_axs[1].set_xlim(0, 5e-4)
-    psd_axs[1].legend(handles=true+fit_ce+fit_xy)
-    psd_axs[0].get_legend().remove()
+    # psd_axs[1].set_ylim(0, 0.5e9)
+    ax.set_xlim(0, 5e-4)
+    ax.legend(handles=true+fit_ce)#+fit_xy)
 
-    axs[1][1].bar(retrieval.midpoints, np.histogram(retrieval.diameters, bins=retrieval.bins)[0], width=0.9*np.diff(retrieval.bins), color="C1", alpha=0.2)
-    axs[1][1].bar(retrieval2.midpoints, np.histogram(retrieval2.diameters, bins=retrieval.bins)[0], width=0.9*np.diff(retrieval.bins), color="C2", alpha=0.2)
-    axs[1][1].set_xlabel("Diameter (m)")
-    axs[1][1].set_ylabel("Count")
+    axs[1].bar(retrieval.midpoints, np.histogram(retrieval.diameters, bins=retrieval.bins)[0], width=0.9*np.diff(retrieval.bins), color="C1", alpha=0.2)
+    # axs[1][1].bar(retrieval2.midpoints, np.histogram(retrieval2.diameters, bins=retrieval.bins)[0], width=0.9*np.diff(retrieval.bins), color="C2", alpha=0.2)
+    axs[1].set_xlabel("Diameter (m)")
+    axs[1].set_ylabel("Count")
 
-    axs[1,0].remove()
+    # plt.tight_layout()
 
-    return fig, (retrieval, retrieval2)
+    return fig, (retrieval)#, retrieval2)
 
 
 
